9fa2fd75b852c2e51a9dff18854c845f310da667
Kirkhammetz <Neofrascati@hotmail.it>
2015-09-06 19:37:25 +0200

    addded method in user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var crypto = require("../lib/crypto");
 
 var Schema = mongoose.Schema;
 
@@ -10,7 +11,24 @@ var UserSchema = new Schema({
 	avatar:String,
 	createdOn:Date,
 	role:String,
-	admin:Boolean
+	admin:Boolean,
+	resetPasswordToken: String,
+  	resetPasswordExpires: Date
 });
+
+// HOOK
+// UserSchema.pre('save',function(next){
+// 	var user = this;
+// 	if (!user.isModified('password')) return next();
+// 	bcrypt.hash(user.password, salt, null, function(err, hash) {
+// 		if (err) return next(err);
+// 		user.password = hash;
+// 		next();
+// 	});
+// })
+// 
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  return crypto.bcrypt.compare(candidatePassword, this.password)
+};
 
 module.exports = mongoose.model("User", UserSchema);
