111d733355bc205ae9e8b1b6f6da65b5c973dce3
Kirkhammetz <Neofrascati@hotmail.it>
2016-01-23 17:57:44 +0100

    fixed semi-disaster

--- admin/assets/js/backend.js
+++ admin/assets/js/backend.js
@@ -1,10 +1,10 @@
 (function($, window, document, undefined){
 	var backend = {
+		max_filesize: 2 * 1000000, // 2MB
 		init:function(){
 			backend.setup();
-			backend.bindEvents();
-			backend.request.init();
 			backend.plugins.init();
+			backend.bindEvents();
 		},
 		setup:function(){
 			/**
@@ -43,6 +43,17 @@
 				$("#media-upload-form").find("input").trigger("click");
 			});
 
+			$('#media-upload-form').submit(function(e) {
+				e.preventDefault();
+			});
+
+			$(".editor").submit(backend.postPageEditor);
+
+			$('#file-upload').change(backend.media_upload);
+
+			$("body").on('click','.remove-media', backend.remove_media)
+
+			//backend.request.remove_media();
 		},
 		plugins:{
 			// pass init function as $.fn so we can call wherever we want it.
@@ -94,165 +105,139 @@
 			 	window.CODE_EDITOR = editor;
 			}
 		},
-		request:{
-			init:function(){
-				backend.request.editorGetData();
-				backend.request.media_upload();
-				backend.request.remove_media();
-			},
-			remove_media: function(){
-				var $button = $(".remove-media");
+		media_upload:function(event){
+			event.preventDefault();
+			event.stopPropagation();
+			
+			var form = new FormData();
 
-				$button.click(function(e){
-					e.preventDefault();
-					var $image = $(this).parent().parent();
-					var image_id = $(this).next().data("id");
+			$button = $("body").find('.upload-file');
 
-					swal({
-						title: "Are you sure?",
-						text: "You will not be able to recover this image!",
-						type: "warning",
-						showCancelButton: true,
-						confirmButtonColor: "#DD6B55",
-						confirmButtonText: "Yes, delete it!",
-						closeOnConfirm: true 
-					}, function(){
-						$.ajax({
-							url: '/api/media/' + image_id,
-							type: 'delete',
-							success: function(res){
-								if(res === "delete") $image.fadeOut();
-							}	
-						});
-					});
+			$button.html('<i class="fa fa-cog fa-spin"></i>');
 
-				});
-			},
-			media_upload:function(){
-				var max_filesize = 2 * 1000000; // 2MB
+			var files = [];
+			var files = $("#media-upload-form").find("input")[0].files;
 
-				$('#media-upload-form').submit(function(e) {
-					e.preventDefault();
-				});
+			for (var i = 0; i < files.length; i++) {
+				if(files[i].size <= backend.max_filesize){
+					form.append('upload', files[i]);
+				}else{
+					toastr.error("File too big (max "+ backend.max_filesize+"):<br>" + files[i].name);
+				}
+			};
 
-				$('#file-upload').change(function(event) {
-					event.preventDefault();
-					event.stopPropagation();
-					
-					var form = new FormData();
+			$.ajax({
+				url: '/api/upload',
+				type: 'POST',
+				cache:false,
+				processData: false,
+				contentType: false,
+				data: form,
+				success: function(res){
+					$button.html('<i class="fa fa-upload"></i>');
+					toastr.success("Media Uploaded");
+					console.log("backend.js :112", res);
+					$('#media-upload-form')[0].reset();
+					setTimeout(function(){
+								window.location.reload();
+					},1000);	
+				}
+			});
+		},
+		postPageEditor:function(e){
+			e.preventDefault();	
+			tinyMCE.triggerSave();
 
-					$button = $("body").find('.upload-file');
-
-					$button.html('<i class="fa fa-cog fa-spin"></i>');
-
-					var files = [];
-					var files = $("#media-upload-form").find("input")[0].files;
-
-					for (var i = 0; i < files.length; i++) {
-						if(files[i].size <= max_filesize){
-							form.append('upload', files[i]);
-						}else{
-							toastr.error("File too big (max "+ max_filesize+"):<br>" + files[i].name);
-						}
+			var contentType = $(".editor").attr("id"),
+					template = $("#templates-choice").val() || "page-template",
+					data = {
+						id: $(".editor").data("id"),
+						title: $(".editor-title").val(),
+						body: $("#text-editor").val(),
+						template:template,
+						tags: []
 					};
+			
+			if(data.title == ""){
+				//DO SOME VALIDATION LATER
+				data.title = "My New Content";
+			}
 
-					$.ajax({
-						url: '/api/upload',
-						type: 'POST',
-						cache:false,
-						processData: false,
-    				contentType: false,
-						data: form,
-						success: function(res){
-							$button.html('<i class="fa fa-upload"></i>');
-							toastr.success("Media Uploaded");
-							console.log("backend.js :112", res);
-							$('#media-upload-form')[0].reset();
-							setTimeout(function(){
-										window.location.reload();
-							},1000);	
-						}
-					});
-					
-				});
-			},
-			editorGetData:function(){				
-				$(".tags-input").click(function(){
-					$(".tags-input").val().split(",")
-				});
-				$(".editor").submit(function(e){
-					e.preventDefault();	
-					tinyMCE.triggerSave();
-
-					var contentType = $(".editor").attr("id"),
-							template = $("#templates-choice").val() || "page-template",
-							data = {
-								id: $(".editor").data("id"),
-								title: $(".editor-title").val(),
-								body: $("#text-editor").val(),
-								template:template,
-								tags: []
-							};
-					
-					console.log("backend.js :390", contentType);
-					console.log("backend.js :389", data);
-
-					if(data.title == ""){
-						//DO SOME VALIDATION LATER
-						data.title = "My New Content";
+			// is a post and is new
+			if(contentType === "editor-post" && data.id === ""){
+				data.tags = $(".tags-input").val().split(",");
+				$.post("/api/post", data,function(res,status){
+					console.log(res);
+					if (status === "success") {
+						toastr.success("Post created!");
+						setTimeout(function() { window.location.replace("/admin/posts"); }, 1000);
 					}
-
-					// is a post and is new
-					if(contentType === "editor-post" && data.id === ""){
-						data.tags = $(".tags-input").val().split(",");
-						$.post("/api/post", data,function(res,status){
-							console.log(res);
-							if (status === "success") {
-								toastr.success("Post created!");
-								setTimeout(function() { window.location.replace("/admin/posts"); }, 1000);
-							}
-						});
-					};
-
-					// is a post and is existing
-					if(contentType === "editor-post" && data.id !== "" ){
-						data.tags = $(".tags-input").val().split(",");
-						$.post("/api/post/" + data.id , data,function(res,status){
-							console.log(status);
-							if (status === "success") {
-								toastr.success("Post edited!");
-								setTimeout(function() { window.location.replace("/admin/posts"); }, 1000);
-							}
-						});
-					};
-
-					// is a page and is new
-					if(contentType === "editor-page" && data.id === ""){
-						data.tags = [];
-						$.post("/api/page", data,function(res,status){
-							console.log(res);
-							if (status === "success") {
-								toastr.success("Page created!");
-								setTimeout(function() { window.location.replace("/admin/pages"); }, 1000);
-							}
-						});
-					};
-
-					// is a page and is existing
-					if(contentType === "editor-page" && data.id !== "" ){
-						data.tags = [];
-						$.post("/api/page/" + data.id, data, function(res,status){
-							console.log(status);
-							if (status === "success") {
-								toastr.success("Page edited!");
-								setTimeout(function() { window.location.replace("/admin/pages"); }, 1000);
-							}
-						});
-					};
-					
 				});
-			},
-		}
+			};
+
+			// is a post and is existing
+			if(contentType === "editor-post" && data.id !== "" ){
+				data.tags = $(".tags-input").val().split(",");
+				$.post("/api/post/" + data.id , data,function(res,status){
+					console.log(status);
+					if (status === "success") {
+						toastr.success("Post edited!");
+						setTimeout(function() { window.location.replace("/admin/posts"); }, 1000);
+					}
+				});
+			};
+
+			// is a page and is new
+			if(contentType === "editor-page" && data.id === ""){
+				data.tags = [];
+				$.post("/api/page", data,function(res,status){
+					console.log(res);
+					if (status === "success") {
+						toastr.success("Page created!");
+						setTimeout(function() { window.location.replace("/admin/pages"); }, 1000);
+					}
+				});
+			};
+
+			// is a page and is existing
+			if(contentType === "editor-page" && data.id !== "" ){
+				data.tags = [];
+				$.post("/api/page/" + data.id, data, function(res,status){
+					console.log(status);
+					if (status === "success") {
+						toastr.success("Page edited!");
+						setTimeout(function() { window.location.replace("/admin/pages"); }, 1000);
+					}
+				});
+			};
+			
+		},
+		remove_media: function(e){
+				e.preventDefault();
+				e.stopPropagation(); 
+				
+				var $image = $(this).parent().parent();
+				var image_id = $(this).next().data("id");
+
+				swal({
+					title: "Are you sure?",
+					text: "You will not be able to recover this image!",
+					type: "warning",
+					showCancelButton: true,
+					confirmButtonColor: "#DD6B55",
+					confirmButtonText: "Yes, delete it!",
+					closeOnConfirm: true 
+				}, function(){
+					$.ajax({
+						url: '/api/media/' + image_id,
+						type: 'delete',
+						success: function(res){
+							if(res === "delete") $image.fadeOut();
+						}	
+					});
+				});
+
+		},
 	}
 
 	/**
